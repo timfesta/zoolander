2ee31fa9bd14433e58cdbdf66e113041a1469554
timfesta <timothyfesta@yahoo.com>
2016-04-10 15:04:07 -0700

    added routes and fixed views

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
 
-  get 'welcome/index'
-
   #users routes
   get 'signup'  =>  'users#new'
   get "/profile", to: 'users#show'
@@ -14,16 +12,21 @@ Rails.application.routes.draw do
   post "session", to: 'sessions#create'
 
   #men routes
+  get 'men' => 'men#index'
   get 'one' => 'men#one'
   get 'two' => 'men#two'
-  get 'men' => 'men#index'
+  get 'three' => 'men#three'
+  get 'four' => 'men#four'
   resources :men
 
   #women routes
   get 'women' => 'women#index'
+  get 'ones' => 'women#one'
+  get 'twos' => 'women#two'
+  get 'threes' => 'women#three'
+  get 'fours' => 'women#four'
   resources :women
 
-  #homepage
   root 'welcome#index'
 
 end
